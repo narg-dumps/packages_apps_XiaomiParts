acf3ed8df06a23c04dd7ca64763165e370f0dd24
Genkzsz11 <Genkzsz11@gmail.com>
2020-05-19 06:52:32 +0700

    CrvsParts: Clean up

--- src/com/corvus/parts/DeviceSettings.java
+++ src/com/corvus/parts/DeviceSettings.java
@@ -45,9 +45,9 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final  String BACKLIGHT_DIMMER_PATH = "/sys/module/mdss_fb/parameters/backlight_dimmer";
 
     public static final String PREF_TORCH_BRIGHTNESS = "torch_brightness";
-    private static final String TORCH_1_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
+    public static final String TORCH_1_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
             "spmi/spmi-0/spmi0-03/800f000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_0/max_brightness";
-    private static final String TORCH_2_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
+    public static final String TORCH_2_BRIGHTNESS_PATH = "/sys/devices/soc/800f000.qcom," +
             "spmi/spmi-0/spmi0-03/800f000.qcom,spmi:qcom,pm660l@3:qcom,leds@d300/leds/led:torch_1/max_brightness";
 
     private static Context mContext;
