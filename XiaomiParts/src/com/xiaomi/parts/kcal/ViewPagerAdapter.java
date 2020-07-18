ea6cb1b7dbac0379dd2de577b9662e59149ed085
narjirolu125 <narjirolu@gmail.com>
2020-07-18 00:31:59 +0000

    mod picture kcal preview
    Signed-off-by: narjirolu125 <narjirolu@gmail.com>

--- XiaomiParts/src/com/xiaomi/parts/kcal/ViewPagerAdapter.java
+++ XiaomiParts/src/com/xiaomi/parts/kcal/ViewPagerAdapter.java
@@ -31,7 +31,7 @@ public class ViewPagerAdapter extends PagerAdapter {
 
     private Context context;
     private LayoutInflater layoutInflater;
-    private Integer [] images = {R.drawable.kcal_image_one};
+    private Integer [] images = {R.drawable.kcal_image_one,R.drawable.kcal_image_two,R.drawable.kcal_image_three};
 
     public ViewPagerAdapter(Context context) {
         this.context = context;
